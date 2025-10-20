5cf041db28a1fdf1b56487f658d67fc037a15df7
kibria1954 <ashadul.kibria1954@gmail.com>
2025-10-20 20:35:15 +0600

    Working with Inputs , Assertion , Annotations, Tagging, Reporters, Screenshots, Before &                                                                       After Hooks

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,24 +1,122 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, chromium } from "@playwright/test";
+import type { Browser, BrowserContext, Page, Locator } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test.skip("has title", async ({}) => {
+//   const browser: Browser = await chromium.launch({
+//     headless: false,
+//     channel: "chrome",
+//   });
+//   const page: Page = await browser.newPage();
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+//   await page.goto("https://www.example.com");
+//   const pageTitle = page.locator("h1");
+//   await expect(pageTitle).toHaveText("Example Domain");
+
+//   await page.waitForTimeout(2000);
+//   await browser.close();
+// });
+
+// test.only("example test", async ({}) => {
+//   const browser: Browser = await chromium.launch({
+//     headless: false,
+//     channel: "chrome",
+//   });
+//   const page: Page = await browser.newPage();
+//   await page.goto("http://zero.webappsecurity.com/");
+//   await page.click(
+//     "//button[@id='signin_button' and contains(@class, 'signin') and contains(@class, 'btn-info')]"
+//   );
+//   await page.click(
+//     "//input[@type='submit' and @name='submit' and @value='Sign in' and contains(@class, 'btn-primary')]"
+//   );
+
+//   const errorMessage = await page.locator("//div[@class ='alert alert-error']");
+//   await expect(errorMessage).toContainText("Login and/or password are wrong.");
+
+//   await page.waitForTimeout(2000);
+//   await browser.close();
+// });
+
+test.describe("My First Test Suite", () => {
+  test("example test", async ({}) => {
+  const browser: Browser = await chromium.launch({
+    headless: false,
+    channel: "chrome",
+  });
+  const page: Page = await browser.newPage();
+  await page.goto("http://zero.webappsecurity.com/");
+  await page.click(
+    "//button[@id='signin_button' and contains(@class, 'signin') and contains(@class, 'btn-info')]"
+  );
+  await page.click(
+    "//input[@type='submit' and @name='submit' and @value='Sign in' and contains(@class, 'btn-primary')]"
+  );
+
+  const errorMessage = await page.locator("//div[@class ='alert alert-error']");
+  await expect(errorMessage).toContainText("Login and/or password are wrong.");
+
+  await page.waitForTimeout(2000);
+  await browser.close();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("Assertions example", async ({}) => {
+  const browser: Browser = await chromium.launch({
+    headless: false,
+    channel: "chrome",
+  });
+  const page: Page = await browser.newPage();
+  await page.goto("https://www.example.com");
+  await expect(page).toHaveURL("https://www.example.com/");
+  await expect(page).toHaveTitle("Example Domain");
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  const element = await page.locator("h1");
+  await expect(element).toBeVisible();
+  await expect(element).toHaveText("Example Domain");
+  await expect(element).toHaveCount(1);
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  const nonExistingElement = await page.locator("h2");
+  await expect(nonExistingElement).not.toBeVisible();
+
+  await page.waitForTimeout(2000);
+  await browser.close();
+});
 });
 
-test("example test", async ({ page }) => {
-  await page.goto("https://example.com");
-  const title = page.locator("h1");
-  await expect(title).toHaveText("Example Domain");
+test.describe.only("Hook", () => {
+  test.beforeEach(async ({}) => {
+    const browser: Browser = await chromium.launch({
+    headless: false,
+    channel: "chrome",
+  });
+  const page: Page = await browser.newPage();
+  await page.goto("https://www.example.com");
+  });
 });
+
+// test.only("has title", async ({}) => {
+//   const browser: Browser = await chromium.launch({
+//     headless: false,
+//     channel: "chrome",
+//   });
+//   const page: Page = await browser.newPage();
+//   await page.goto("https://playwright.dev/");
+
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
+
+// test("example test", async ({ page }) => {
+//   await page.goto("https://example.com");
+//   const title = page.locator("h1");
+//   await expect(title).toHaveText("Example Domain");
+// });
